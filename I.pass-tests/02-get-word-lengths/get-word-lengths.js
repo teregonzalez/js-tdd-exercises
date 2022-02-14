e26fe541dc9e67e642c4669c04aed82ef21f1666
teregonzalez <teregonzalezpro@gmail.com>
2022-02-14 15:26:24 -0300

    Test for exercice2

--- I.pass-tests/02-get-word-lengths/get-word-lengths.js
+++ I.pass-tests/02-get-word-lengths/get-word-lengths.js
@@ -1,3 +1,11 @@
-var getWordLengths = function(someWords) {};
+const getWordLengths = (someWords) => {
+    let numArray = []
+    someWords.forEach(
+        element =>  
+        numArray.push(element.split('').length));
+    return numArray;
+};
 
-module.exports = getWordLengths;
+module.exports = {
+    getWordLengths
+};
--- I.pass-tests/02-get-word-lengths/get-word-lengths.test.js
+++ I.pass-tests/02-get-word-lengths/get-word-lengths.test.js
@@ -1,9 +1,15 @@
-var wordLengths = require("./get-word-lengths");
+const { getWordLengths } = require("./get-word-lengths");
 
-test("Get word lengths", function() {
-  var words = ["sun", "potato", "roundabout", "pizza"];
-  var expected = [3, 6, 10, 5];
+describe("Function getWordLengths", () => {
+  it('Giving ["sun", "potato", "roundabout", "pizza"] to the function should return [3, 6, 10, 5]', () => {
+    //Arrange
+    const words = ["sun", "potato", "roundabout", "pizza"];
+    const expected = [3, 6, 10, 5];
 
-  var output = wordLengths(words);
-  expect(output).toEqual(expected);
+    //Act
+    const output = getWordLengths(words);
+
+    //Assert
+    expect(output).toEqual(expected);
+  });
 });
