109ddf4d82819b118be251a4e7f6cc681dfbb54c
teregonzalez <teregonzalezpro@gmail.com>
2022-02-15 14:42:46 -0300

    Se agrega test de función factorial

--- I.pass-tests/05-factorial/factorial.test.js
+++ I.pass-tests/05-factorial/factorial.test.js
@@ -11,31 +11,54 @@ describe("Function factorial", () => {
 
     //Assert
     expect(output).toBe(exp1);
+  });
 
-    /*
-    var in2 = 9;
-    var exp2 = 362880;
+  it("Giving 9 should return 362880", () => {
+    //Arrange
+    const in2 = 9;
+    const exp2 = 362880;
 
-    var in3 = 1;
-    var exp3 = 1;
+    //Act
+    const out2 = factorial(in2);
 
-    var in4 = 0;
-    var exp4 = 1;
-
-    var in5 = 3;
-    var exp5 = 6;
-
-    var out1 = factorial(in1);
-    var out2 = factorial(in2);
-    var out3 = factorial(in3);
-    var out4 = factorial(in4);
-    var out5 = factorial(in5);
-
-    expect(out1).toEqual(exp1);
+    //Assert
     expect(out2).toEqual(exp2);
+  });
+
+  it("Giving 1 should return 1", () => {
+    //Arrange
+    const in3 = 1;
+    const exp3 = 1;
+
+    //Act
+    const out3 = factorial(in3);
+
+    //Assert
     expect(out3).toEqual(exp3);
+  });
+
+  it("Giving 0 should return 0", () => {
+    //Arrange
+    const in4 = 0;
+    const exp4 = 0;
+
+    //Act
+    const out4 = factorial(in4);
+
+    //Assert
     expect(out4).toEqual(exp4);
+  });
+
+  it("Giving 3 should return 6", () => {
+    //Arrange
+    const in5 = 3;
+    const exp5 = 6;
+
+    //Act
+    const out5 = factorial(in5);
+
+    //Assert
     expect(out5).toEqual(exp5);
-    */
+
   });
 });
