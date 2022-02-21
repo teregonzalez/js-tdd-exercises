7151b96655e46e08b2c0f0738d269366511d88fb
teregonzalez <teregonzalezpro@gmail.com>
2022-02-21 17:54:02 -0300

    Se corrige test largerThanTen

--- II.write-tests/04-numbers-greater-than-10/numbersGreaterThan10.test.js
+++ II.write-tests/04-numbers-greater-than-10/numbersGreaterThan10.test.js
@@ -6,9 +6,14 @@ describe("largerThanTen function", () => {
         //Arrange
         const input = [4, 10, 32, 9, 21];
         const output = [32, 21];
+
+        //Act
+        const result = largerThanTen(input);
+
+        //Assert
+        expect(result).toEqual(output)
     });
 });
-test("Get numbers greater than 10", function() {});
 
 // input: [4, 10, 32, 9, 21];
 // expected output: [32, 21];
