ed7bb29e05ece436b3797d989c35596d34cd8617
teregonzalez <teregonzalezpro@gmail.com>
2022-02-21 17:52:22 -0300

    Se agrega test para ejercicio 3

--- II.write-tests/04-numbers-greater-than-10/numbersGreaterThan10.test.js
+++ II.write-tests/04-numbers-greater-than-10/numbersGreaterThan10.test.js
@@ -1,5 +1,13 @@
-var largerThanTen = require("./numbersGreaterThan10");
 
+const {largerThanTen} = require("./numbersGreaterThan10");
+
+describe("largerThanTen function", () => {
+    it('Giving the array [4, 10, 32, 9, 21], would return [32, 21]', () => {
+        //Arrange
+        const input = [4, 10, 32, 9, 21];
+        const output = [32, 21];
+    });
+});
 test("Get numbers greater than 10", function() {});
 
 // input: [4, 10, 32, 9, 21];
