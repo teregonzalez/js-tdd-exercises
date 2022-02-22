c6c78f1bde4590d02f5a0a8477491df70e09632a
teregonzalez <teregonzalezpro@gmail.com>
2022-02-22 16:19:33 -0300

    Se agrega test para largestNumber

--- II.write-tests/05-get-second-third-smallest/get-second-third.test.js
+++ II.write-tests/05-get-second-third-smallest/get-second-third.test.js
@@ -1,3 +1,33 @@
+const {getSecondThird} = require("./get-second-third");
+
+describe("Function getSecondThird", () => {
+    it("Giving [90, 5, 11, 8, 6] should return [6, 8]", () => {
+        //Arrange
+        const input = [90, 5, 11, 8, 6];
+        const output = [6, 8];
+
+        //Act
+        const result = getSecondThird(input);
+        
+        //Assert 
+        expect(result).toEqual(output);
+    });
+
+    it("Giving [90, 5, 11, 8, 6] should return [6, 8]", () => {
+        //Arrange
+        const input = [1,4,3,7,8];
+        const output = [3,4];
+        const original = [1,4,3,7,8];
+
+        //Act
+        const result = getSecondThird(input);
+        
+        //Assert 
+        expect(result).toEqual(output);
+        expect(input).toEqual(original);
+    });
+});
+
 // example
 // input = [90, 5, 11, 8, 6];
 // expected output = [6, 8];
--- II.write-tests/06-largest-number/largest-number.test.js
+++ II.write-tests/06-largest-number/largest-number.test.js
@@ -1,3 +1,33 @@
+const {getLargestNumber} = require("./largest-number");
+
+describe("Function getLargestNumber", () => {
+    it("Giving [3, 21, 88, 4, 36] should return 88", () => {
+        //Arrange
+        const input = [3, 21, 88, 4, 36];
+        const expected = 88;
+
+        //Act
+        const result = getLargestNumber(input);
+
+        //Assert
+        expect(result).toEqual(expected);
+    })
+
+    it("Giving [4, 5, 40, 9] should return 40 and the array remains the same", () => {
+        //Arrange
+        const input = [4, 5, 40, 9];
+        const expected = 40;
+        const original = [4, 5, 40, 9];
+
+        //Act
+        const result = getLargestNumber(input);
+
+        //Assert
+        expect(result).toEqual(expected);
+        expect(input).toEqual(original);
+    })
+})
+
 // example
 // input: [3, 21, 88, 4, 36];
 // expected: 88;
