f8883ff158fb73983e34f75ccf26aa968a5f47c2
teregonzalez <teregonzalezpro@gmail.com>
2022-03-03 15:51:03 -0300

    cambios

--- III.tdd-katas/fizz-buzz/fizz-buzz.test.js
+++ III.tdd-katas/fizz-buzz/fizz-buzz.test.js
@@ -1,7 +1,7 @@
-const {fizzbuzz} = require("./fizz-buzz");
+const { fizzbuzz } = require('./fizz-buzz');
 
-describe("Function fizzbuzz", () => {
-  it("Giving 1 should return 1, Fizz", () => {
+describe('Function fizzbuzz', () => {
+  it('Giving 1 should return 1, Fizz', () => {
     //Arrange
     const number = 1;
 
@@ -9,10 +9,10 @@ describe("Function fizzbuzz", () => {
     const result = fizzbuzz(number);
 
     //Assert
-    expect(result).toEqual("1");
+    expect(result).toEqual('1');
   });
 
-  it("Giving 2 should return '1, 2' ", () => {
+  it('Giving 2 should return "1, 2" ', () => {
     //Arrange
     const number = 2;
 
@@ -22,5 +22,38 @@ describe("Function fizzbuzz", () => {
     //Assert
     expect(result).toEqual("1, 2");
   });
+
+  it('Giving 7 should return "1, 2, fizz, 4, buzz, 6" ', () => {
+    //Arrange
+    const number = 7;
+
+    //Act
+    const result = fizzbuzz(number);
+
+    //Assert
+    expect(result).toEqual('1, 2, Fizz, 4, Buzz, Fizz, 7');
+  });
+  it('Giving 15 should return "1, 2, fizz, 4, buzz, 6, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz" ', () => {
+    //Arrange
+    const number = 15;
+
+    //Act
+    const result = fizzbuzz(number);
+
+    //Assert
+    expect(result).toEqual('1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz');
+  });
+  it('Giving an empy parameter should return an error', () => {
+    //Arrange
+    const number = '';
+
+    //Act
+    const result = () => fizzbuzz(number);
+
+    //Assert
+    expect(result).toThrow("Error, debes ingresar un número")
+
+  })
+
 });
 
