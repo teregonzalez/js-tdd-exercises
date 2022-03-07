25de92b2d3cdd9d250c3dbf46320e0c2b0bae794
teregonzalez <teregonzalezpro@gmail.com>
2022-03-07 18:08:55 -0300

    Se agrega for a fizzBuzz

--- III.tdd-katas/fizz-buzz/fizz-buzz.js
+++ III.tdd-katas/fizz-buzz/fizz-buzz.js
@@ -2,7 +2,14 @@ const fizzbuzz = (number) => {
     if(number.length === 0){
         throw new Error('Error, debes ingresar un número');
     }
-    const numbers = Array.from(Array(number).keys()).map(number => number+1)
+
+    let numbersArray = [];
+    for(let i = number; i>0; i--){
+        numbersArray.push(i)
+    }
+    
+    const numbers = numbersArray.sort((acumulador, number) => {return acumulador-number},0)
+    //const numbers = Array.from(Array(number).keys()).map(number => number+1)
     const numbersArr = numbers.map(number=> {
         if(number%3===0 && number%5===0){
         return 'FizzBuzz'
--- III.tdd-katas/password-verifier/passwordVerifier.js
+++ III.tdd-katas/password-verifier/passwordVerifier.js
@@ -1,3 +1,7 @@
+class PasswordVerifier {
+    verify()
+}
+
 const verify = (password) => {
     if(password.length<8){
         throw new Error('Password is too short. must contain 8 digits')
