27512defc26707f3dc19920279489f256832c69e
teregonzalez <teregonzalezpro@gmail.com>
2022-03-09 11:40:36 -0300

    Test y funcion passwordVerifier

--- III.tdd-katas/password-verifier/passwordVerifier.js
+++ III.tdd-katas/password-verifier/passwordVerifier.js
@@ -1,21 +1,74 @@
 class PasswordVerifier {
-    verify()
+
+    // Crea un objeto a partir de una clase
+    constructor(password){
+        this.password = password;
+        this.errores = [];
+    }
+
+    
+    verify() {
+        let acumulador = 2;
+
+        try {
+            this.isNotNull();
+            this.itContainsLowercaseLetter();
+            try {
+                this.isLengthLargerThan(8);
+                acumulador++;
+            } catch (error) {
+                this.errores.push(error.message);
+            }
+            try {
+                this.itContainsUppercaseLetter();
+                acumulador++;
+            } catch (error) {
+                this.errores.push(error.message);
+            }
+            try {
+                this.itContainsANumber();
+                acumulador++;
+            } catch (error) {
+                this.errores.push(error.message);
+            }
+            if(acumulador>=3){
+                return 'Ok'
+            }else{
+                return 'Not ok'
+            }
+        } catch (error) {
+            this.errores.push(error.message);
+            return 'Not ok'
+        }
+    }
+
+    isNotNull() {
+        if(this.password === null){
+            throw new Error('Password is null')
+        }
+    }
+
+    isLengthLargerThan(limit){
+        if(this.password.length<limit){
+            throw new Error('Password is too short. must contain 8 digits')
+        }
+    }
+
+    itContainsLowercaseLetter(){
+        if(this.password.search(/[a-z]/) === -1){
+            throw new Error('Password must contain a lowercase letter')
+        }
+    }
+    itContainsUppercaseLetter(){
+        if(this.password.search(/[A-Z]/) === -1){
+            throw new Error('Password must contain an uppercase letter')
+        }
+    }
+    itContainsANumber(){
+        if(this.password.search(/\d+/g) === -1){
+            throw new Error('Password must contain a number')
+        }
+    }
 }
 
-const verify = (password) => {
-    if(password.length<8){
-        throw new Error('Password is too short. must contain 8 digits')
-    }if(password.search(/[a-z]/) === -1){
-        throw new Error('Password must contain a letter')
-    }if(password.search(/[A-Z]/) === -1){
-        throw new Error('Password must contain an uppercase letter')
-    }
-    if(password.search(/\d+/g) === -1){
-        throw new Error('Password must contain a number')
-    }
-    return 'Ok'
-}
-
-module.exports = {
-    verify
-};
+module.exports = PasswordVerifier;
--- III.tdd-katas/password-verifier/passwordVerifier.test.js
+++ III.tdd-katas/password-verifier/passwordVerifier.test.js
@@ -1,64 +1,102 @@
-const { verify } = require('./passwordVerifier');
+const PasswordVerifier = require('./passwordVerifier');
 
-describe('Verify function', () => {
-    it('Giving "Hello1" as a password should return "OK"', () => {
+describe('PasswordVerfifier class test', () => {
+    it('Should exist', () => {
+        // Arrange
+        const passwordVerifier = new PasswordVerifier('');
+        // Act
+        const isInstanceOfPasswordVerifier = passwordVerifier instanceof PasswordVerifier; // Confirma el tipo de un objeto en tiempo de ejecución
+        // Assert
+        expect(isInstanceOfPasswordVerifier).toBe(true);
+    });
+    it('Giving a null password should throw an error', () => {
+        // Arrange
+        const passwordVerifier = new PasswordVerifier(null);
+        // Act and assert
+        expect(() => passwordVerifier.isNotNull()).toThrow('Password is null');
+    });
+    it('Giving a 6 length password should throw an error', () => {
+        // Arrange
+        const passwordVerifier = new PasswordVerifier('123456');
+        const limit = 8;
+        // Act and assert
+        expect(() => passwordVerifier.isLengthLargerThan(limit)).toThrow('Password is too short. must contain 8 digits');
+    });
+
+    it('Giving 12345678 should throw an error', () =>{
+        // Arrange
+        const passwordVerifier = new PasswordVerifier('12345678');
+        // Act and assert
+        expect(() => passwordVerifier.itContainsLowercaseLetter()).toThrow('Password must contain a lowercase letter');
+    });
+
+    it('Giving 12345678e should throw an error', () =>{
+        // Arrange
+        const passwordVerifier = new PasswordVerifier('12345678e');
+        // Act and assert
+        expect(() => passwordVerifier.itContainsLowercaseLetter()).not.toThrow('Password must contain a lowercase letter');
+    });
+
+    it('Giving 1234567o should throw an error', () =>{
+        // Arrange
+        const passwordVerifier = new PasswordVerifier('1234567o');
+
+        // Act and assert
+        expect(() => passwordVerifier.itContainsUppercaseLetter()).toThrow('Password must contain an uppercase letter');
+    });
+
+    it('Giving Hellohello should throw an error', () => {
+        // Arrange
+        const passwordVerifier = new PasswordVerifier('Hellohello');
+
+        // Act and asser
+        expect(() => passwordVerifier.itContainsANumber()).toThrow('Password must contain a number');
+    })
+
+    it('Giving H5678976 should return "Not ok"', () => {
         //Arrange
-        const password = 'Hello123';
+        const passwordVerifier = new PasswordVerifier('H678976');
 
         //Act
-        const result = verify(password);
-
-        //Assert
-        expect(result).toEqual('Ok')
+        expect(passwordVerifier.verify()).toBe('Not ok');
     });
-    it('Giving "12345678" should throw an error', () => {
+
+    it('Giving Hu1234 should return "Ok"', () => {
         //Arrange
-        const password = '12345678';
+        const passwordVerifier = new PasswordVerifier('Hu1234');
 
-        //Act
-        const result = () => verify(password);
-
-        //Assert
-        expect(result).toThrow('Password must contain a letter');
+        //Act and Assert
+        expect(passwordVerifier.verify()).toBe('Ok');
     });
-    it('Giving "123456eT" should return "Ok"', () => {
+    it('Giving u1234567 should return "Ok"', () => {
         //Arrange
-        const password = '123456eT';
+        const passwordVerifier = new PasswordVerifier('u1234567');
 
-        //Act
-        const result = verify(password);
-
-        //Assert
-        expect(result).toBe('Ok')
+        //Act and assert
+        expect(passwordVerifier.verify()).toBe('Ok');
     });
-    it('Giving "123456" should throw an error', () => {
+
+    it('Giving HolaHolaaa should return "Ok"', () => {
         //Arrange
-        const password = '123456';
+        const passwordVerifier = new PasswordVerifier('HolaHolaaa');
 
-        //Act
-        const result = () => verify(password);
-
-        //Assert
-        expect(result).toThrow('Password is too short. must contain 8 digits');
+        //Act and assert
+        expect(passwordVerifier.verify()).toBe('Ok');
     });
-    it('Giving "123456et" should throw an error', () => {
+
+    it('Giving null should return "Not ok"', () => {
         //Arrange
-        const password = '123456et';
+        const passwordVerifier = new PasswordVerifier(null);
 
-        //Act
-        const result = () => verify(password);
-
-        //Assert
-        expect(result).toThrow('Password must contain an uppercase letter');
+        //Act and assert
+        expect(passwordVerifier.verify()).toBe('Not ok');
     });
-    it('Giving "Passwordd" should throw an error', () => {
+
+    it('Giving Contrasena should return "Ok"', () => {
         //Arrange
-        const password = 'Passwordd';
-
-        //Act
-        const result = () => verify(password);
-
-        //Assert
-        expect(result).toThrow('Password must contain a number');
-    });
+        const passwordVerifier = new PasswordVerifier('Contrasena');
+        
+        //acts and assert
+        expect(passwordVerifier.verify()).toBe('Ok');
+    })
 });
